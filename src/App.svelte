6874451cd4b273df9906191965f51e0e9c9cd4b2
David Callizaya <davidcallizaya@gmail.com>
2021-03-02 20:23:09 -0400

    Add inputs and output

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,32 @@
 <script>
+  let input = "";
+  let input_unit = "";
+  let target_unit = "";
+  let response = "";
 </script>
 
 <main>
   <h1>Welcome to Unit Conversions</h1>
+  <div class="flex">
+    <input
+      bind:value={input}
+      placeholder="Input numerical value"
+      autofocus
+      type="number"
+    />
+    <select bind:value={input_unit}>
+      <option value="" />
+    </select>
+    <select bind:value={target_unit}>
+      <option value="" />
+    </select>
+    <input bind:value={response} placeholder="Student response" type="number" />
+    <input
+      class="output"
+      placeholder="Output"
+      readonly
+    />
+  </div>
 </main>
 
 <style>
@@ -24,5 +48,12 @@
     main {
       max-width: none;
     }
+  }
+
+  .flex {
+    display: flex;
+  }
+  .output {
+    font-weight: bold;
   }
 </style>
